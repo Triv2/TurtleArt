8178121622c5180d5bcc6c9a5ec41cc9cba6da0b
Trivial-lore <triviallore@gmail.com>
2023-07-06 22:34:42 -0500

    b

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 import random
 
-class Turtle(Turtle):
-  def __init__(self,shape,color,x,y):
-    self.shape= shape
-    self.color= color
-    self.x= x
-    self.y= y
+# class Turtle(Turtle):
+#   def __init__(self,shape,color,x,y):
+#     self.shape= shape
+#     self.color= color
+#     self.x= x
+#     self.y= y
     
-  def place(self,x,y):
-    self.x= x
+#   def place(self,x,y):
+#     self.x= x
     
 
 class Grid:
@@ -17,10 +17,20 @@ class Grid:
     self.height= height
     self.width= width
     
-timmy_turtle = Turtle()
-timmy_turtle.shape("turtle")
 
-max_height= input("Enter the max height of the grid: ")
-max_width = input("Enter the max width of the grid: ")
+# max_height= input("Enter the max height of the grid: ")
+# max_width = input("Enter the max width of the grid: ")
 
-grid =Grid(max_height,max_width)
+# grid =Grid(max_height,max_width)
+
+timmy= Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+screen= Screen()
