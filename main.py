391f0ed4a6521082aba365889fd7502fda671b10
Trivial-lore <triviallore@gmail.com>
2023-07-07 10:44:18 -0500

    Art project fully working without OOP, needs to be converted into class OOP

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from turtle import Turtle, Screen
 import random
 
-
-
 shape = "circle"
 icons = []
 max_height=250
@@ -10,13 +8,10 @@ max_width=250
 total_size = max_height*max_width
 
 # class Turtle(Turtle):
-#   def __init__(self,shape,color,x,y):
+#   def __init__(self,shape,color):
 #     self.shape= shape
 #     self.color= color
-#     self.x= x
-#     self.y= y
     
-   
 
 # class Grid:
 #   def __init__(self,height,width):
@@ -32,20 +27,19 @@ total_size = max_height*max_width
 #         turtle.setx(x)
 #         turtle.sety(y)
 #         icons.append(turtle)
- 
 
 # grid =Grid(max_height,max_width)
 # turtle= Turtle(shape,color,0,0)
-
 # grid.place(turtle,max_height,max_width)
+timmy= Turtle()
 
 for x in range(-250,max_width,50):
   for y in range(-250,max_height,50):
     color = random.choice(["red", "green", "blue", "yellow", "purple", "orange"])
-    timmy= Turtle()
     timmy.penup()
     timmy.shape("circle")
     timmy.color(color)
     timmy.setx(x)
     timmy.sety(y)
+    timmy.stamp()
 screen= Screen()
