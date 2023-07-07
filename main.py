7040813509e3358d01eb70c03dca5ea93f2ff268
Trivial-lore <triviallore@gmail.com>
2023-07-07 10:33:00 -0500

    art project works, need to add smoother movement

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from turtle import Turtle, Screen
 import random
 
+
+
+shape = "circle"
+icons = []
+max_height=250
+max_width=250
+total_size = max_height*max_width
+
 # class Turtle(Turtle):
 #   def __init__(self,shape,color,x,y):
 #     self.shape= shape
@@ -8,29 +16,36 @@ import random
 #     self.x= x
 #     self.y= y
     
-#   def place(self,x,y):
-#     self.x= x
-    
+   
 
-class Grid:
-  def __init__(self,height,width):
-    self.height= height
-    self.width= width
+# class Grid:
+#   def __init__(self,height,width):
+#     self.height= height
+#     self.width= width
+#   def move(self,turtle,movement,max_height,max_width):
+#     turtle.forward(movement)
     
-
-# max_height= input("Enter the max height of the grid: ")
-# max_width = input("Enter the max width of the grid: ")
+#   def place(self,turtle,max_height,max_width):
+#     for y in range(max_height):
+#       for x in range(max_width):
+#         turtle= Turtle(shape,color,x,y)
+#         turtle.setx(x)
+#         turtle.sety(y)
+#         icons.append(turtle)
+ 
 
 # grid =Grid(max_height,max_width)
+# turtle= Turtle(shape,color,0,0)
 
-timmy= Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
+# grid.place(turtle,max_height,max_width)
+
+for x in range(-250,max_width,50):
+  for y in range(-250,max_height,50):
+    color = random.choice(["red", "green", "blue", "yellow", "purple", "orange"])
+    timmy= Turtle()
+    timmy.penup()
+    timmy.shape("circle")
+    timmy.color(color)
+    timmy.setx(x)
+    timmy.sety(y)
 screen= Screen()
